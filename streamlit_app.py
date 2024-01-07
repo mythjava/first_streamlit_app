af2abd1d182c9419da3dc5e45c94c1f0842625d6
mythjava <33082411+mythjava@users.noreply.github.com>
2024-01-07 22:59:59 +0800

    Update streamlit_app.py
    added Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.write('Omega 3 & Blueberry Oatmeal')
-streamlit.write('Kale, Spinach & Rocket Smoothie')
-streamlit.write('Hard-Boiled FreeRange Egg')
-streamlit.text('This is text and above is write')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
